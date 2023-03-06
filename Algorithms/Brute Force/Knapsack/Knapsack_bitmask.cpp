df154358738b3069c8b6aa723d1ac505618b7de9
Bn_Gaber <99830416+Abdelrhman-Sayed70@users.noreply.github.com>
2023-03-06 03:47:36 +0200

    Update Knapsack_bitmask.cpp

--- Algorithms/Brute Force/Knapsack/Knapsack_bitmask.cpp
+++ Algorithms/Brute Force/Knapsack/Knapsack_bitmask.cpp
@@ -1,23 +1,7 @@
 #include<bits/stdc++.h>
-#include<unordered_map>
 using namespace std;
 #define fastIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0) 
-#define ll long long
 #define el cout << "\n" 
-#define endl "\n"
-#define yes cout <<"YES\n" 
-#define no cout << "NO\n"
-#define all(v) v.begin(),v.end()
-#define out(v) for(auto it : v) { cout << it << " ";}
-const double pi = 2 * acos(0.0);
-ll mod = 1e9 + 7;
-void files() {
-#pragma warning(disable : 4996).
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-}
 //*********************
 int getBit(int n, int index) {
     return ((n >> index) & 1);
@@ -26,7 +10,7 @@ void you_can_do_it() {
     int n, w;
     cin >> n >> w;
     vector<pair<int, int>>v(n); // weight, value
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cin >> v[i].first >> v[i].second;
     }
     // bitmask from 0 to (2^n)-1
